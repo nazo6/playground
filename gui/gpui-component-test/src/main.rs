52e98123a90b3cf3e2a119505c144715b8526010
nazo6 <git@nazo6.dev>
2025-02-02 23:42:46 +0900

    update

--- gui/gpui-component-test/src/main.rs
+++ gui/gpui-component-test/src/main.rs
@@ -1,52 +1,67 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use gpui::*;
-use ui::{button::Button, init};
+use tokio::sync::mpsc::UnboundedReceiver;
+use ui::init;
 
 struct Root {
-    count: i64,
+    message: SharedString,
+    _recv_task: Task<()>,
 }
 
-impl Render for Root {
-    fn render(&mut self, window: &mut Window, cx: &mut Context<'_, Self>) -> impl IntoElement {
-        div()
-            .flex()
-            .flex_col()
-            .h_full()
-            .w_full()
-            .justify_center()
-            .items_center()
-            .bg(white())
-            .child(
-                div()
-                    .flex()
-                    .child(Button::new("minus").label("-").on_mouse_down(
-                        MouseButton::Left,
-                        cx.listener(|this, _, _, cx| {
-                            this.count -= 1;
-                            cx.notify();
-                        }),
-                    ))
-                    .child(Button::new("plus").label("+").on_mouse_down(
-                        MouseButton::Left,
-                        cx.listener(|this, _, _, cx| {
-                            this.count += 1;
-                            cx.notify();
-                        }),
-                    )),
-            )
-            .child(format!("Count: {}", self.count))
+impl Root {
+    fn new(cx: &mut Context<'_, Self>, mut mes_receiver: UnboundedReceiver<String>) -> Self {
+        let _recv_task = cx.spawn(|this, mut cx| async move {
+            loop {
+                if let Some(message) = mes_receiver.recv().await {
+                    println!("Received in UI: {}", message);
+                    this.update(&mut cx, |this, cx| {
+                        this.message = SharedString::new(message);
+                        cx.notify();
+                    })
+                    .unwrap();
+                };
+            }
+        });
+
+        Self {
+            message: SharedString::new(""),
+            _recv_task,
+        }
     }
 }
 
-fn main() -> Result<()> {
+impl Render for Root {
+    fn render(&mut self, _window: &mut Window, cx: &mut Context<'_, Self>) -> impl IntoElement {
+        println!("Render");
+
+        div()
+            .bg(white())
+            .child(format!("message: {}", self.message))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let chan = tokio::sync::mpsc::unbounded_channel();
+
+    tokio::spawn(async move {
+        let mut i = 0;
+        loop {
+            tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+            i += 1;
+            chan.0.send(format!("from tokio task: {}", i)).unwrap();
+            println!("Sending from tokio task: {}", i);
+        }
+    });
+
     let app = Application::new();
 
-    app.run(move |cx| {
-        init(cx);
+    app.run(move |app| {
+        init(app);
 
-        cx.open_window(WindowOptions::default(), |_, cx| {
-            cx.new(|_| Root { count: 0 })
+        app.open_window(WindowOptions::default(), |_, app| {
+            app.new(|cx| Root::new(cx, chan.1))
         })
         .unwrap();
     });
